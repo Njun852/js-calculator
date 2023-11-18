78baa1bca22930ec315fd55a61a11a97e5e880c2
Njun852 <njunjuntillaz@gmail.com>
2023-11-18 18:32:05 +0800

    Finished the app but still need refactoring

--- script.js
+++ script.js
@@ -1,92 +1,58 @@
-function calculate(str){
+const expressionBtns = document.querySelectorAll(".expression")
+const upperOutput = document.querySelector("#upper")
+const lowerOutput = document.querySelector("#lower")
 
-    let expression = str.trim()
-    let a = ""
-    let b = ""
-    let operator
-    let end
-    if(expression.length <= 0){
-        return 0
-    }
-    for(let i = 0; i < expression.length; i++){
-        if(operator == null && /[*/+-/^]/.test(expression[i]) && i != 0 && expression[i] != "."){
-            operator = expression[i]
-            continue
-        }
-        if(operator != null){
-            if(/[*/(]/.test(expression[i])){
-                foundOperator = false
-                let parenthesis = []
-                let newExpression = ""
-                for(let i = expression.indexOf(operator)+1; i < expression.length; i++){
-                    if(foundOperator && /\D/.test(expression[i]) || expression[i] != ".") {
-                        end = i              
-                        break
-                    }
-
-                    if(expression[i] == "("){
-                        parenthesis.push(i)
-                        continue
-                    }
-                    if(expression[i] == ")"){
-                        if(parenthesis[parenthesis.length-1] == "("){
-                            parenthesis.pop()
-                            if(parenthesis.length <= 0) break
-                        }
-                    }
-
-                    if(/[*/+-/^]/.test(expression[i]) && expression[i] != ".") {
-                        foundOperator = true
-                    }
-                  
-                    newExpression += expression[i]
-                }
-                b = calculate(newExpression)
-                break
-            }
-            if(expression[i] == ")"){
-                end = i+1
-                break
-            }
-            b += expression[i]
-            continue
-        }
-
-        if(/[()]/.test(expression[i])) continue
-        a += expression[i]
-    }
-    let removeAt = ""
-    if(end == null) removeAt = expression
-    for(let i = 0; i < end; i++){
-        removeAt += expression[i]
-    }
-    a = Number(a)
-    b = Number(b)
-    if(operator == null) return a
-    let equals = 0
-    switch(operator){
-
-        case "+":
-            equals = a+b
-            break
-        case "-":
-            equals = a-b
-            break
-        case "/":
-            equals = a/b
-            break
-        case "*":
-            equals = a*b
-            break
-        case "^":
-            equals = Math.pow(a, b)
-            break
-        default:
-            return "Something went wrong"
-    }
-    expression= expression.replace(removeAt, "")
-
-    if(expression.length > 0) return calculate(equals+expression)
-    return equals
-  
+const equal = document.querySelector("#equal")
+const clear = document.querySelector("#clear")
+const del = document.querySelector("#del")
+let expression = "";
+for(let i = 0; i < expressionBtns.length; i++){
+    expressionBtns[i].addEventListener("click", ()=>{
+        resetOutputTxtStyle()
+        if(expressionBtns[i].textContent == "0" && expression == "") return
+        if(operators.test(expressionBtns[i].textContent) && 
+        operators.test(expression[expression.length-1])) return
+        inputExpression(expressionBtns[i].innerText)
+    })
 }
+
+function inputExpression(input){
+    if(getComputedStyle(lowerOutput).getPropertyValue("display") == "none")
+    lowerOutput.style.display = "block"
+
+    expression += input
+    upperOutput.textContent = expression
+    lowerOutput.textContent = calculate(expression)
+}
+
+equal.addEventListener("click", ()=>{
+    upperOutput.style.fontSize = "1.7rem"
+    lowerOutput.style.fontSize = "3rem"
+    lowerOutput.style.opacity = "1"
+    expression = calculate(expression)
+})
+
+function resetOutputTxtStyle(){
+    upperOutput.style.fontSize = "3rem"
+    lowerOutput.style.fontSize = "1.7rem"
+    lowerOutput.style.opacity = "0.7"
+}
+function absoluteClear(){
+    lowerOutput.style.display = "none"
+    expression = ""
+    upperOutput.textContent = "0"
+}
+clear.addEventListener("click", ()=>{
+    resetOutputTxtStyle()
+    absoluteClear()
+})
+del.addEventListener("click", ()=>{
+    expression = expression.split("")
+    expression.pop()
+    expression = expression.join("")
+    if(expression == ""){
+        absoluteClear()
+        return
+    }
+    inputExpression("")
+})
